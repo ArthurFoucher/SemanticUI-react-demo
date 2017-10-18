415f0f467c3e08969d0ea3f9ed4c00fa71229dd8
Arthur Foucher <foucherarthur@gmail.com>
2017-10-18 17:55:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a dummy longin/signup page using Semantic-UI-React.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+## Nice to remember:
+* Install `npm install semantic-ui-react semantic-ui-css --save`
+* `import "semantic-ui-css/semantic.css";` to get the styling
+* [https://react.semantic-ui.com/](https://react.semantic-ui.com/) to get the props for each component
+* Grid component for responsive design
---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Grid, Form, Input, Button } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Grid columns={1} textAlign="center" className="App">
+        <Grid.Column width="6">
+          <Grid
+            columns={2}
+            verticalAlign="middle"
+            relaxed="very"
+            divided
+            stackable
+          >
+            <Grid.Column textAlign="center">
+              <Button
+                icon="signup"
+                color="green"
+                size="big"
+                content="Login"
+                labelPosition="left"
+              />
+            </Grid.Column>
+            <Grid.Column textAlign="left">
+              <Form>
+                <Form.Field>
+                  <label>Username</label>
+                  <Input
+                    icon="user"
+                    placeholder="Username"
+                    iconPosition="left"
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Password</label>
+                  <Input icon="lock" type="password" iconPosition="left" />
+                </Form.Field>
+                <Button type="submit" color="blue">
+                  Login
+                </Button>
+              </Form>
+            </Grid.Column>
+          </Grid>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default App;
